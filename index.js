6385485457582868406fbd875447ca04a5e79818
Hudi_Indrawan <79536179+Kororo212@users.noreply.github.com>
2022-06-04 10:51:29 +0700

    test

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = (x, z => (z (x).map((item, index) => `${index+1}. ${item}`) );
+const sorter = (x, z)=> (z (x).map((item, index) => `${index+1}. ${item}`) );
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
 const sortAscending = (x) => {
