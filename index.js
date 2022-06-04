41b4ed5900f37a6bf643d6547516b0fae259aafd
Hudi_Indrawan <79536179+Kororo212@users.noreply.github.com>
2022-06-04 10:33:04 +0700

    test

--- index.js
+++ index.js
@@ -6,15 +6,19 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = const sorter = (z,xCend)=>(xCend(z).map((name,i)=>`${i+1}.${name}`).sort((a,b)=>a-b));
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (z)=>{
+  return x.sort();
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (x)=>{
+  return x.sort().reverse();
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
